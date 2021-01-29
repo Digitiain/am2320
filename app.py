64c6affa493fe33ea923d436cd744e0cdacb328b
Digitiain <iain@digital.mod.uk>
2021-01-29 17:47:00 +0000

    Bug: fix production setup

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
     if os.environ.get("FLASK_ENV") == "development":
         return render_template('index.html', temperature="21", humidity="56")
     else: 
-        sensor_readings = am2320.readSensor()
+        sensor_readings = am2320.get_sensor_data()
         temperature = sensor_readings[0]
         humidity = sensor_readings[1]
         return render_template('index.html', temperature=temperature, humidity=humidity)
