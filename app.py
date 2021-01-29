b635eb1096d60bfa38e3c0c0ac7700de611db63b
Digitiain <iain@digital.mod.uk>
2021-01-29 12:13:50 +0000

    Main page with test and real data
    Style for main page done at a basic level.
    Test data for when FLASK_ENV=development.
    Real data for when it's not (assumes sensor is set up).

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+import os
 from flask import Flask, render_template
+
+if os.environ.get("FLASK_ENV") != "development":
+    import am2320
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+
+    if os.environ.get("FLASK_ENV") == "development":
+        return render_template('index.html', temperature="21", humidity="56")
+    else: 
+        sensor_readings = am2320.readSensor()
+        temperature = sensor_readings[0]
+        humidity = sensor_readings[1]
+        return render_template('index.html', temperature=temperature, humidity=humidity)
 
 
 if __name__ == '__main__':
